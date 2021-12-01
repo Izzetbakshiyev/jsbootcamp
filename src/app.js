f01dcb037f16616a18e75405187e9bc4eef0f948
Izzet <izzet.baxshiyev13@gmail.com>
2021-12-01 12:13:29 +0400

    18 Ekim Des Sonu

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
 
-let dolarDun = 9.20
-dolarDun = "9.20"
+let dolarBugun = 9.30
+let dolarDun = "Ankara"
+
 {
     let dolarDun = 9.10
 }
@@ -12,14 +11,20 @@ dolarDun = "9.20"
 console.log(dolarDun)
 
 const euroDun = 11.2
-//euroDun = 11
+//euroDun = 1
 
 console.log(euroDun)
 
 //array
 //camelCasing
 //PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+let konutKredileri = 
+[
+"Konut Kredisi",
+"Emlak Konut Kredisi",
+"Kamu Konut Kredisi",
+"Ozel Konut Kredisi"
+]
 
 console.log("<ul>")
 for(let i = 0;i<konutKredileri.length;i++){
@@ -27,10 +32,5 @@ for(let i = 0;i<konutKredileri.length;i++){
 }
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
 console.log(konutKredileri)
